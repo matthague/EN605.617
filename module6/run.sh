67fe320c0552e266000a30b22da967d3aafd555a
t <t>
2021-10-06 19:01:55 -0400

    inti

---
+++ module6/run.sh
@@ -0,0 +1,16 @@
+cmake .
+make -j
+
+./assignment 1 1 1 1
+
+echo -e "\n\nDEMO 0\n"
+./assignment
+
+echo -e "\n\nDEMO 1\n"
+./assignment 1024 1024 8192
+
+echo -e "\n\nDEMO 2\n"
+./assignment 256 256 1
+
+echo -e "\n\nDEMO 3\n"
+./assignment 65536 1024 128
