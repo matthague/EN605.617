8af42af2855b677d478907c83ccda0de8ddbca44
t <t>
2021-10-30 14:03:12 -0400

    closer

---
+++ module9/run_nppNvgraph.sh
@@ -0,0 +1,11 @@
+make nppNvgraphAssignment
+
+echo -e "\n\nNPP/NVGRAPH DEMO 0\n"
+./nppNvgraphAssignment
+
+
+echo -e "\n\nNPP/NVGRAPH DEMO 1\n"
+./nppNvgraphAssignment lena.pgm 128
+
+echo -e "\n\nNPP/NVGRAPH DEMO 2\n"
+./nppNvgraphAssignment lena.pgm 128
---
+++ module9/run_thrust.sh
@@ -0,0 +1,10 @@
+make thrustAssignment
+
+echo -e "\n\nTHRUST DEMO 0\n"
+./thrustAssignment
+
+echo -e "\n\nTHRUST DEMO 1\n"
+./thrustAssignment 512 512
+
+echo -e "\n\nTHRUST DEMO 2\n"
+./thrustAssignment 524288 32
