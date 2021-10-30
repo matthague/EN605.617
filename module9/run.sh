8b0777f3b5ad0ef48b8726e6a3f7f293d94af36a
t <t>
2021-10-30 13:58:14 -0400

    files

--- module9/run.sh
+++ module9/run.sh
@@ -1,19 +1,23 @@
-make -j
+make thrustAssignment
 
-./assignment 1 1 1 1
+echo -e "\n\nTHRUST DEMO 0\n"
+./thrustAssignment
 
-echo -e "\n\nDEMO 0\n"
-./assignment
+echo -e "\n\nTHRUST DEMO 1\n"
+./thrustAassignment 512 512
 
-echo -e "\n\nDEMO 1\n"
-./assignment 512 512
-
-echo -e "\n\nDEMO 2\n"
-./assignment 1024 32
-
-echo -e "\n\nDEMO 3\n"
-./assignment 128 256
+echo -e "\n\nTHRUST DEMO 2\n"
+./assignment 524288 32
 
 
-echo -e "\n\nDEMO 4\n"
-./assignment 1024 1024
+make nppNvgraphAssignment
+
+echo -e "\n\nNPP/NVGRAPH DEMO 0\n"
+./nppNvgraphAssignment
+
+
+echo -e "\n\nNPP/NVGRAPH DEMO 1\n"
+./nppNvgraphAssignment lena.pgm 128
+
+echo -e "\n\nNPP/NVGRAPH DEMO 2\n"
+./nppNvgraphAssignment lena.pgm 128
