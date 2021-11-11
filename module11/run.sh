ad2d6d35748b247e0a6ee2059104c9005f6750b9
t <t>
2021-11-11 12:53:48 -0500

    lets test

---
+++ module11/run.sh
@@ -0,0 +1,10 @@
+make assignment
+
+echo -e "\Convolution Modification Demo 0\n"
+./assignment
+
+echo -e "\Convolution Modification Demo 1\n"
+./assignment
+
+echo -e "\Convolution Modification Demo 2\n"
+./assignment 
