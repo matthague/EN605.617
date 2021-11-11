afaeac8654d8b016c1118adc62497c4a49bab041
t <t>
2021-11-11 13:25:54 -0500

    format

--- module11/assignment.cpp
+++ module11/assignment.cpp
@@ -18,289 +18,276 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <chrono>
 
 #ifdef __APPLE__
-  #include <OpenCL/cl.h>
+#include <OpenCL/cl.h>
 #else
-  #include <CL/cl.h>
+
+#include <CL/cl.h>
+
 #endif
 
 #if !defined(CL_CALLBACK)
-  #define CL_CALLBACK
+#define CL_CALLBACK
 #endif
 
 // Constants
-const unsigned int inputSignalWidth  = 49;
+const unsigned int inputSignalWidth = 49;
 const unsigned int inputSignalHeight = 49;
 cl_uint inputSignal[inputSignalHeight][inputSignalWidth];
 
-const unsigned int maskWidth  = 7;
+const unsigned int maskWidth = 7;
 const unsigned int maskHeight = 7;
 cl_float mask[maskHeight][maskWidth] =
-{
-  {.25, .25, .33333, .33333, .33333, .25, .25},
-	{.25, .33333, .5, .5, .5, .33333, .25},
-	{.33333, .5, 1, 1, 1, .5, .33333},
-	{.33333, .5, 1, 0, 1, .5, .33333},
-	{.33333, .5, 1, 1, 1, .5, .33333},
-	{.25, .33333, .5, .5, .5, .33333, .25},
-	{.25, .25, .33333, .33333, .33333, .25, .25}
-};
+        {
+                {.25,    .25,    .33333, .33333, .33333, .25,    .25},
+                {.25,    .33333, .5,     .5,     .5,     .33333, .25},
+                {.33333, .5,     1,      1,      1,      .5,     .33333},
+                {.33333, .5,     1,      0,      1,      .5,     .33333},
+                {.33333, .5,     1,      1,      1,      .5,     .33333},
+                {.25,    .33333, .5,     .5,     .5,     .33333, .25},
+                {.25,    .25,    .33333, .33333, .33333, .25,    .25}
+        };
 
-const unsigned int outputSignalWidth  = inputSignalWidth - (maskWidth - 1);
+const unsigned int outputSignalWidth = inputSignalWidth - (maskWidth - 1);
 const unsigned int outputSignalHeight = inputSignalHeight - (maskHeight - 1);
 cl_uint outputSignal[outputSignalHeight][outputSignalWidth];
 
-///
 // Function to check and handle OpenCL errors
 inline void
-checkErr(cl_int err, const char * name)
-{
+checkErr(cl_int err, const char *name) {
     if (err != CL_SUCCESS) {
-        std::cerr << "ERROR: " <<  name << " (" << err << ")" << std::endl;
+        std::cerr << "ERROR: " << name << " (" << err << ")" << std::endl;
         exit(EXIT_FAILURE);
     }
 }
 
 void CL_CALLBACK contextCallback(
-	const char * errInfo,
-	const void * private_info,
-	size_t cb,
-	void * user_data)
-{
-	std::cout << "Error occured during context use: " << errInfo << std::endl;
-	// should really perform any clearup and so on at this point
-	// but for simplicitly just exit.
-	exit(1);
+        const char *errInfo,
+        const void *private_info,
+        size_t cb,
+        void *user_data) {
+    std::cout << "Error occured during context use: " << errInfo << std::endl;
+    exit(1);
 }
 
-///
-//	main() for Convoloution example
-//
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     cl_int errNum;
     cl_uint numPlatforms;
-	cl_uint numDevices;
-    cl_platform_id * platformIDs;
-	cl_device_id * deviceIDs;
+    cl_uint numDevices;
+    cl_platform_id *platformIDs;
+    cl_device_id *deviceIDs;
     cl_context context = NULL;
-	cl_command_queue queue;
-	cl_program program;
-	cl_kernel kernel;
-	cl_mem inputSignalBuffer;
-	cl_mem outputSignalBuffer;
-	cl_mem maskBuffer;
+    cl_command_queue queue;
+    cl_program program;
+    cl_kernel kernel;
+    cl_mem inputSignalBuffer;
+    cl_mem outputSignalBuffer;
+    cl_mem maskBuffer;
 
-  // Set input signal as a random integer between 0 and 99
-  srand(time(0));
-  for(unsigned int i = 0; i < inputSignalHeight; i++) {
-    for(unsigned int j = 0; j < inputSignalWidth; j++) {
-      inputSignal[i][j] = rand() % 100;
+    // Set input signal as a random integer between 0 and 99
+    srand(time(0));
+    for (unsigned int i = 0; i < inputSignalHeight; i++) {
+        for (unsigned int j = 0; j < inputSignalWidth; j++) {
+            inputSignal[i][j] = rand() % 100;
+        }
     }
-  }
 
     // First, select an OpenCL platform to run on.
-	errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
-	checkErr(
-		(errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS),
-		"clGetPlatformIDs");
+    errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
+    checkErr(
+            (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS),
+            "clGetPlatformIDs");
 
-	platformIDs = (cl_platform_id *)alloca(
-       		sizeof(cl_platform_id) * numPlatforms);
+    platformIDs = (cl_platform_id *) alloca(
+            sizeof(cl_platform_id) * numPlatforms);
 
     errNum = clGetPlatformIDs(numPlatforms, platformIDs, NULL);
     checkErr(
-	   (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS),
-	   "clGetPlatformIDs");
+            (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS),
+            "clGetPlatformIDs");
 
-	// Iterate through the list of platforms until we find one that supports
-	// a GPU device, otherwise fail with an error.
-	deviceIDs = NULL;
-	cl_uint i;
-	for (i = 0; i < numPlatforms; i++)
-	{
-		errNum = clGetDeviceIDs(
-            platformIDs[i],
-            CL_DEVICE_TYPE_GPU,
-            0,
-            NULL,
-            &numDevices);
-		if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND)
-	    {
-			checkErr(errNum, "clGetDeviceIDs");
+    // Find a GPU device
+    deviceIDs = NULL;
+    cl_uint i;
+    for (i = 0; i < numPlatforms; i++) {
+        errNum = clGetDeviceIDs(
+                platformIDs[i],
+                CL_DEVICE_TYPE_GPU,
+                0,
+                NULL,
+                &numDevices);
+        if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND) {
+            checkErr(errNum, "clGetDeviceIDs");
+        } else if (numDevices > 0) {
+            deviceIDs = (cl_device_id *) alloca(sizeof(cl_device_id) * numDevices);
+            errNum = clGetDeviceIDs(
+                    platformIDs[i],
+                    CL_DEVICE_TYPE_GPU,
+                    numDevices,
+                    &deviceIDs[0],
+                    NULL);
+            checkErr(errNum, "clGetDeviceIDs");
+            break;
         }
-	    else if (numDevices > 0)
-	    {
-		   	deviceIDs = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
-			errNum = clGetDeviceIDs(
-				platformIDs[i],
-				CL_DEVICE_TYPE_GPU,
-				numDevices,
-				&deviceIDs[0],
-				NULL);
-			checkErr(errNum, "clGetDeviceIDs");
-			break;
-	   }
-	}
+    }
 
-    // Next, create an OpenCL context on the selected platform.
+    // Create an OpenCL context
     cl_context_properties contextProperties[] =
-    {
-        CL_CONTEXT_PLATFORM,
-        (cl_context_properties)platformIDs[i],
-        0
-    };
+            {
+                    CL_CONTEXT_PLATFORM,
+                    (cl_context_properties) platformIDs[i],
+                    0
+            };
     context = clCreateContext(
-		contextProperties,
-		numDevices,
-        deviceIDs,
-		&contextCallback,
-		NULL,
-		&errNum);
-	checkErr(errNum, "clCreateContext");
+            contextProperties,
+            numDevices,
+            deviceIDs,
+            &contextCallback,
+            NULL,
+            &errNum);
+    checkErr(errNum, "clCreateContext");
 
-	std::ifstream srcFile("assignment.cl");
+    std::ifstream srcFile("assignment.cl");
     checkErr(srcFile.is_open() ? CL_SUCCESS : -1, "reading assignment.cl");
 
-	std::string srcProg(
-        std::istreambuf_iterator<char>(srcFile),
-        (std::istreambuf_iterator<char>()));
+    std::string srcProg(
+            std::istreambuf_iterator<char>(srcFile),
+            (std::istreambuf_iterator<char>()));
 
-	const char * src = srcProg.c_str();
-	size_t length = srcProg.length();
+    const char *src = srcProg.c_str();
+    size_t length = srcProg.length();
 
-	// Create program from source
-	program = clCreateProgramWithSource(
-		context,
-		1,
-		&src,
-		&length,
-		&errNum);
-	checkErr(errNum, "clCreateProgramWithSource");
+    // Create program from source
+    program = clCreateProgramWithSource(
+            context,
+            1,
+            &src,
+            &length,
+            &errNum);
+    checkErr(errNum, "clCreateProgramWithSource");
 
-	// Build program
-	errNum = clBuildProgram(
-		program,
-		numDevices,
-		deviceIDs,
-		NULL,
-		NULL,
-		NULL);
-    if (errNum != CL_SUCCESS)
-    {
+    // Build program
+    errNum = clBuildProgram(
+            program,
+            numDevices,
+            deviceIDs,
+            NULL,
+            NULL,
+            NULL);
+    if (errNum != CL_SUCCESS) {
         // Determine the reason for the error
         char buildLog[16384];
         clGetProgramBuildInfo(
-			program,
-			deviceIDs[0],
-			CL_PROGRAM_BUILD_LOG,
-            sizeof(buildLog),
-			buildLog,
-			NULL);
+                program,
+                deviceIDs[0],
+                CL_PROGRAM_BUILD_LOG,
+                sizeof(buildLog),
+                buildLog,
+                NULL);
 
         std::cerr << "Error in kernel: " << std::endl;
         std::cerr << buildLog;
-		checkErr(errNum, "clBuildProgram");
+        checkErr(errNum, "clBuildProgram");
     }
 
-	// Create kernel object
-	kernel = clCreateKernel(
-		program,
-		"convolve",
-		&errNum);
-	checkErr(errNum, "clCreateKernel");
+    // Create kernel object
+    kernel = clCreateKernel(
+            program,
+            "convolve",
+            &errNum);
+    checkErr(errNum, "clCreateKernel");
 
-	// Now allocate buffers
-	inputSignalBuffer = clCreateBuffer(
-		context,
-		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-		sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
-		static_cast<void *>(inputSignal),
-		&errNum);
-	checkErr(errNum, "clCreateBuffer(inputSignal)");
+    // Now allocate buffers
+    inputSignalBuffer = clCreateBuffer(
+            context,
+            CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+            sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
+            static_cast<void *>(inputSignal),
+            &errNum);
+    checkErr(errNum, "clCreateBuffer(inputSignal)");
 
-	maskBuffer = clCreateBuffer(
-		context,
-		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-		sizeof(cl_float) * maskHeight * maskWidth,
-		static_cast<void *>(mask),
-		&errNum);
-	checkErr(errNum, "clCreateBuffer(mask)");
+    maskBuffer = clCreateBuffer(
+            context,
+            CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+            sizeof(cl_float) * maskHeight * maskWidth,
+            static_cast<void *>(mask),
+            &errNum);
+    checkErr(errNum, "clCreateBuffer(mask)");
 
-	outputSignalBuffer = clCreateBuffer(
-		context,
-		CL_MEM_WRITE_ONLY,
-		sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
-		NULL,
-		&errNum);
-	checkErr(errNum, "clCreateBuffer(outputSignal)");
+    outputSignalBuffer = clCreateBuffer(
+            context,
+            CL_MEM_WRITE_ONLY,
+            sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
+            NULL,
+            &errNum);
+    checkErr(errNum, "clCreateBuffer(outputSignal)");
 
-	// Pick the first device and create command queue.
-	queue = clCreateCommandQueue(
-		context,
-		deviceIDs[0],
-		0,
-		&errNum);
-	checkErr(errNum, "clCreateCommandQueue");
+    // Pick the first device and create command queue.
+    queue = clCreateCommandQueue(
+            context,
+            deviceIDs[0],
+            0,
+            &errNum);
+    checkErr(errNum, "clCreateCommandQueue");
 
-    errNum  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
-	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
+    errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
+    errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
     errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
-	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
-	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth);
-	checkErr(errNum, "clSetKernelArg");
+    errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
+    errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth);
+    checkErr(errNum, "clSetKernelArg");
 
-	const size_t globalWorkSize[2] = { outputSignalWidth, outputSignalHeight };
-    const size_t localWorkSize[2]  = { 1, 1 };
+    const size_t globalWorkSize[2] = {outputSignalWidth, outputSignalHeight};
+    const size_t localWorkSize[2] = {1, 1};
 
     // Start the clock
     auto t1 = high_resolution_clock::now();
 
     // Queue the kernel up for execution across the array
     errNum = clEnqueueNDRangeKernel(
-		queue,
-		kernel,
-		2,
-		NULL,
-        globalWorkSize,
-		localWorkSize,
-        0,
-		NULL,
-		NULL);
-	checkErr(errNum, "clEnqueueNDRangeKernel");
+            queue,
+            kernel,
+            2,
+            NULL,
+            globalWorkSize,
+            localWorkSize,
+            0,
+            NULL,
+            NULL);
+    checkErr(errNum, "clEnqueueNDRangeKernel");
 
-  // Stop the clock
+    // Stop the clock
     auto t2 = high_resolution_clock::now();
     duration<double, std::milli> kernel_time = t2 - t1;
 
-  // read the results
-	errNum = clEnqueueReadBuffer(
-		queue,
-		outputSignalBuffer,
-		CL_TRUE,
-        0,
-		sizeof(cl_uint) * outputSignalHeight * outputSignalHeight,
-		outputSignal,
-        0,
-		NULL,
-		NULL);
-	checkErr(errNum, "clEnqueueReadBuffer");
+    // read the results
+    errNum = clEnqueueReadBuffer(
+            queue,
+            outputSignalBuffer,
+            CL_TRUE,
+            0,
+            sizeof(cl_uint) * outputSignalHeight * outputSignalHeight,
+            outputSignal,
+            0,
+            NULL,
+            NULL);
+    checkErr(errNum, "clEnqueueReadBuffer");
 
     // Output the result buffer
-    for (int y = 0; y < outputSignalHeight; y++)
-	{
-		for (int x = 0; x < outputSignalWidth; x++)
-		{
-			std::cout << outputSignal[y][x] << " ";
-		}
-		std::cout << std::endl;
-	}
+    for (int y = 0; y < outputSignalHeight; y++) {
+        for (int x = 0; x < outputSignalWidth; x++) {
+            std::cout << outputSignal[y][x] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     std::cout << std::endl << "Executed program succesfully." << std::endl;
 
     // Output timing info
-    std:cout << "Kernel took: " << kernel_time.count() << " (ms)" << std::endl;
+    std:
+    cout << "Kernel took: " << kernel_time.count() << " (ms)" << std::endl;
 
-	return 0;
+    return 0;
 }
