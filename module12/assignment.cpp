9c6af897c5e23cb8dfbd1f7e7bef62ec855dbc9c
t <t>
2021-11-18 21:14:46 -0500

    test

--- module12/assignment.cpp
+++ module12/assignment.cpp
@@ -149,7 +149,7 @@ int main(int argc, char **argv) {
     for (unsigned int i = 0; i < queues.size(); i++) {
         cl_event event;
 
-        size_t gWI = 6;
+        size_t gWI = 4;
 
         errNum = clEnqueueNDRangeKernel(
                 queues[i],
