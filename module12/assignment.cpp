5e6dc3b890fe64f0785818d64485bed0629f7499
t <t>
2021-11-18 21:04:07 -0500

    git

--- module12/assignment.cpp
+++ module12/assignment.cpp
@@ -149,7 +149,7 @@ int main(int argc, char **argv) {
     for (unsigned int i = 0; i < queues.size(); i++) {
         cl_event event;
 
-        size_t gWI = NUM_BUFFER_ELEMENTS;
+        //size_t gWI = NUM_BUFFER_ELEMENTS;
         size_t gWI = NUM_SUBBUFFER_ELEMENTS * NUM_BUFFER_ELEMENTS; // 2x2
 
         errNum = clEnqueueNDRangeKernel(
