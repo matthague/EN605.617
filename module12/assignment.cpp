15829a547111eba623b8c5631faed9d7402aba4a
t <t>
2021-11-18 22:05:25 -0500

    ref

--- module12/assignment.cpp
+++ module12/assignment.cpp
@@ -33,9 +33,9 @@ int main(int argc, char **argv) {
     cl_device_id *deviceIDs;
     cl_context context;
     cl_program program;
-    std::vector <cl_kernel> kernels;
-    std::vector <cl_command_queue> queues;
-    std::vector <cl_mem> buffers;
+    std::vector<cl_kernel> kernels;
+    std::vector<cl_command_queue> queues;
+    std::vector<cl_mem> buffers;
     int *inputOutput;
 
     int platform = DEFAULT_PLATFORM;
@@ -99,39 +99,41 @@ int main(int argc, char **argv) {
     }
 
     // Create a single buffer to cover all the input data
-    cl_mem main_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices, NULL, &errNum);
+    cl_mem main_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+                                        NULL, &errNum);
     checkErr(errNum, "clCreateBuffer");
 
     // Now for each device, create NUM_SUBBUFFERS subbuffers
     for (unsigned int i = 0; i < numDevices; i++) {
-      for(unsigned int j = 0; j < NUM_SUBBUFFERS; j++) {
-        cl_buffer_region region = {
-                NUM_SUBBUFFER_ELEMENTS * i * sizeof(int),
-                NUM_SUBBUFFER_ELEMENTS * sizeof(int)
-        };
-        cl_mem buffer = clCreateSubBuffer(main_buffer, CL_MEM_READ_WRITE, CL_BUFFER_CREATE_TYPE_REGION, &region, &errNum);
-        checkErr(errNum, "clCreateSubBuffer");
+        for (unsigned int j = 0; j < NUM_SUBBUFFERS; j++) {
+            cl_buffer_region region = {
+                    NUM_SUBBUFFER_ELEMENTS * i * sizeof(int),
+                    NUM_SUBBUFFER_ELEMENTS * sizeof(int)
+            };
+            cl_mem buffer = clCreateSubBuffer(main_buffer, CL_MEM_READ_WRITE, CL_BUFFER_CREATE_TYPE_REGION, &region,
+                                              &errNum);
+            checkErr(errNum, "clCreateSubBuffer");
 
-        buffers.push_back(buffer);
-      }
+            buffers.push_back(buffer);
+        }
     }
 
     // Create command queues
     for (unsigned int i = 0; i < numDevices; i++) {
-      for(unsigned int j = 0; j < NUM_SUBBUFFERS; j++) {
-        cl_command_queue queue = clCreateCommandQueue(context, deviceIDs[i], 0, &errNum);
-        checkErr(errNum, "clCreateCommandQueue");
+        for (unsigned int j = 0; j < NUM_SUBBUFFERS; j++) {
+            cl_command_queue queue = clCreateCommandQueue(context, deviceIDs[i], 0, &errNum);
+            checkErr(errNum, "clCreateCommandQueue");
 
-        queues.push_back(queue);
+            queues.push_back(queue);
 
-        cl_kernel kernel = clCreateKernel(program, "average_helper", &errNum);
-        checkErr(errNum, "clCreateKernel(average_helper)");
+            cl_kernel kernel = clCreateKernel(program, "average_helper", &errNum);
+            checkErr(errNum, "clCreateKernel(average_helper)");
 
-        errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &buffers[i*numDevices + j]);
-        checkErr(errNum, "clSetKernelArg(average_helper)");
+            errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &buffers[i * numDevices + j]);
+            checkErr(errNum, "clSetKernelArg(average_helper)");
 
-        kernels.push_back(kernel);
-      }
+            kernels.push_back(kernel);
+        }
     }
 
     // Display input data
@@ -145,70 +147,71 @@ int main(int argc, char **argv) {
 
     // Write input data
     errNum = clEnqueueWriteBuffer(
-          queues[queues.size() - 1],
-          main_buffer,
-          CL_TRUE,
-          0,
-          sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-          (void *) inputOutput,
-          0,
-          NULL,
-          NULL);
+            queues[queues.size() - 1],
+            main_buffer,
+            CL_TRUE,
+            0,
+            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+            (void *) inputOutput,
+            0,
+            NULL,
+            NULL);
 
-    std::vector <cl_event> events;
+    std::vector<cl_event> events;
 
     // Start the clock
     auto t1 = std::chrono::high_resolution_clock::now();
 
     // call kernel for each subbuffer on each device
     for (unsigned int i = 0; i < numDevices; i++) {
-      for(unsigned int j = 0; j < NUM_SUBBUFFERS; j++) {
-        cl_event event;
+        for (unsigned int j = 0; j < NUM_SUBBUFFERS; j++) {
+            cl_event event;
 
-        size_t lWI = NUM_SUBBUFFER_ELEMENTS;
-        size_t offset = (j * NUM_SUBBUFFER_ELEMENTS);
+            size_t lWI = NUM_SUBBUFFER_ELEMENTS;
+            size_t offset = (j * NUM_SUBBUFFER_ELEMENTS);
 
-        errNum = clEnqueueNDRangeKernel(
-                queues[i],
-                kernels[i*numDevices + j],
-                1,
-                &offset,
-                (const size_t *) &lWI,
-                (const size_t *) &lWI,
-                0,
-                0,
-                &event);
+            errNum = clEnqueueNDRangeKernel(
+                    queues[i],
+                    kernels[i * numDevices + j],
+                    1,
+                    &offset,
+                    (const size_t *) &lWI,
+                    (const size_t *) &lWI,
+                    0,
+                    0,
+                    &event);
 
-        events.push_back(event);
+            events.push_back(event);
+        }
     }
-  }
 
     // Technically don't need this as we are doing a blocking read with in-order queue.
     clWaitForEvents(events.size(), &events[0]);
 
     // Stop the clock
     auto t2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1);
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
 
     // Read back computed data
     clEnqueueReadBuffer(
-          queues[queues.size() - 1],
-          main_buffer,
-          CL_TRUE,
-          0,
-          sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-          (void *) inputOutput,
-          0,
-          NULL,
-          NULL);
+            queues[queues.size() - 1],
+            main_buffer,
+            CL_TRUE,
+            0,
+            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+            (void *) inputOutput,
+            0,
+            NULL,
+            NULL);
 
     // Display average
     for (unsigned i = 0; i < numDevices; i++) {
-        float average = ((float)inputOutput[i*NUM_BUFFER_ELEMENTS]) /NUM_BUFFER_ELEMENTS; // Divide as a float for accuracy
+        float average =
+                ((float) inputOutput[i * NUM_BUFFER_ELEMENTS]) / NUM_BUFFER_ELEMENTS; // Divide as a float for accuracy
         std::cout << "AVERAGE VALUE: " << average;
         std::cout << std::endl;
     }
-    
+
     // Output timing info
     std::cout << "Kernel took: " << duration.count() << " (microseconds)" << std::endl;
 
--- module12/run.sh
+++ module12/run.sh
@@ -1,10 +1,10 @@
 make assignment
 
-echo -e "\n Subbuffer Modification Demo 0\n"
+echo -e "\n[!] Subbuffer Modification Demo 0 [!]\n"
 ./assignment
 
-echo -e "\n Subbuffer Modification Demo 1\n"
+echo -e "\n[!] Subbuffer Modification Demo 1 [!]\n"
 ./assignment
 
-echo -e "\n Subbuffer Modification Demo 2\n"
+echo -e "\n[!] Subbuffer Modification Demo 2 [!]\n"
 ./assignment
